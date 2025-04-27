1546ac3a6de44cd8d92a1ee452544813a922a669
Prabhat Kumar Singh <46450896+p4prabhu@users.noreply.github.com>
2025-04-27 16:55:31 -0400

    Time: 909 ms (82.45%), Space: 0B (100%) - LeetHub

---
+++ 1415-students-and-examinations/1415-students-and-examinations.sql
@@ -0,0 +1,58 @@
+-- # Write your MySQL query statement below
+-- -- select st.student_id,  st.student_name,su.subject_name , count(e.subject_name) as attended_exams
+-- -- from Students st
+-- -- join subjects su
+-- -- left join examinations e
+-- -- on st.student_id=e.student_id and su.subject_name=e.subject_name
+-- -- group by st.student_id, su.subject_name
+-- -- order by st.student_id, su.subject_name
+
+
+
+
+-- select s.student_id, s.student_name, e.subject_name, 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- select s.student_id, s.student_name, su.subject_name, count(e.student_id) as attended_exams 
+-- from students s join 
+-- subjects su left join 
+-- Examinations e on
+-- s.student_id=e.student_id and e.subject_name=su.subject_name
+-- group by s.student_id, su.subject_name
+-- order by s.student_id, su.subject_name
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+select s.student_id, s.student_name, su.subject_name, count(e.student_id) attended_exams
+ from Students s  join Subjects su 
+left join Examinations e on
+s.student_id=e.student_id and e.subject_name=su.subject_name
+group by s.student_id, su.subject_name
+order by s.student_id, su.subject_name
