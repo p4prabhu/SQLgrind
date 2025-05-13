53b134ef574c66d3ae0b95be4c29455e27862921
Prabhat Kumar Singh <46450896+p4prabhu@users.noreply.github.com>
2025-05-12 21:07:48 -0400

    Time: 579 ms (37.88%), Space: 0B (100%) - LeetHub

---
+++ 1161-project-employees-i/1161-project-employees-i.sql
@@ -0,0 +1,40 @@
+-- -- # Write your MySQL query statement below
+-- -- select a.project_id , round(avg(b.experience_years),2) as average_years
+-- -- from Project a left join Employee b on a.employee_id=b.employee_id
+-- -- GROUP BY a.project_id;
+
+
+
+-- -- select p.project_id, round(avg(e.experience_years),2) as average_years from project p
+-- -- left join employee e on p.employee_id=e.employee_id
+-- -- group by p.project_id
+
+
+
+
+
+
+-- select p.project_id, ifnull(round(avg(e.experience_years),2),0) average_years from project p left join employee e on e.employee_id=p.employee_id
+-- group by project_id
+-- order by project_id
+
+
+
+-- select project_id, ifnull(round(avg(experience_years),2),0)average_years from employee e left join project p on e.employee_id=p.employee_id where project_id !='null'
+-- group by project_id
+-- order by project_id
+
+
+
+
+
+
+
+select p.project_id, ifnull(round(avg(e.experience_years),2),0) average_years from Employee e join Project p
+on p.employee_id=e.employee_id
+group by p.project_id
+order by p.project_id
+
+
+
+
