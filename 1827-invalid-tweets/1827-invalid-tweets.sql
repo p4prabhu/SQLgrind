297529c0d8723803f23dfdcdb862f471e8f1c73a
Prabhat Kumar Singh <46450896+p4prabhu@users.noreply.github.com>
2025-07-16 06:46:17 -0400

    Time: 635 ms (27.63%), Space: 0B (100%) - LeetHub

---
+++ 1827-invalid-tweets/1827-invalid-tweets.sql
@@ -0,0 +1,23 @@
+# Write your MySQL query statement below
+
+-- select tweet_id from tweets
+-- where length(content)>15
+
+
+
+
+
+
+
+
+
+
+select tweet_id from tweets
+where length(content)>15
+
+
+
+
+
+
+
