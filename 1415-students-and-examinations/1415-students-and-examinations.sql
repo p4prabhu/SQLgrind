4f72e37937bb4093a3b84e9afcf8f9ff9db295b8
Prabhat Kumar Singh <46450896+p4prabhu@users.noreply.github.com>
2025-07-20 13:31:19 -0400

    Time: 955 ms (40.52%), Space: 0B (100%) - LeetHub

--- 1415-students-and-examinations/1415-students-and-examinations.sql
+++ 1415-students-and-examinations/1415-students-and-examinations.sql
@@ -50,21 +50,19 @@
 
 
 
-select s.student_id, s.student_name, su.subject_name, count(e.student_id) attended_exams
- from Students s  join Subjects su 
-left join Examinations e on
-s.student_id=e.student_id and e.subject_name=su.subject_name
-group by s.student_id, su.subject_name
-order by s.student_id, su.subject_name
+-- select s.student_id, s.student_name, su.subject_name, count(e.student_id) attended_exams
+--  from Students s  join Subjects su 
+-- left join Examinations e on
+-- s.student_id=e.student_id and e.subject_name=su.subject_name
+-- group by s.student_id, su.subject_name
+-- order by s.student_id, su.subject_name
 
 
 
 
 
-
-
-
-
-
-
-
+select st.student_id, st.student_name, su.subject_name, count(ex.student_id) as attended_exams
+from students st join Subjects su
+left join Examinations ex on st.student_id=ex.student_id and ex.subject_name=su.subject_name
+group by st.student_id, su.subject_name
+order by st.student_id, su.subject_name
