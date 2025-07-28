9c9d880f4cd30d41e037aa1fed3e8270730df8e2
Prabhat Kumar Singh <46450896+p4prabhu@users.noreply.github.com>
2025-07-27 22:18:13 -0400

    Time: 1076 ms (5%), Space: 0B (100%) - LeetHub

--- 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -59,7 +59,16 @@
 
 
 
-select name from Employee where id in ( select managerId from Employee group by managerId having count(*)>=5)
+-- select name from Employee where id in ( select managerId from Employee group by managerId having count(*)>=5)
+
+
+
+
+
+
+select name from Employee where id in (select managerId from Employee
+group by managerId
+having count(*)>=5 )
 
 
 
