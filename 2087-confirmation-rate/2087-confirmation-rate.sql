fcd43dbb16ace858022dd8fc52530a0589868835
Prabhat Kumar Singh <46450896+p4prabhu@users.noreply.github.com>
2025-07-27 22:25:26 -0400

    Time: 633 ms (68.16%), Space: 0B (100%) - LeetHub

--- 2087-confirmation-rate/2087-confirmation-rate.sql
+++ 2087-confirmation-rate/2087-confirmation-rate.sql
@@ -13,15 +13,17 @@
 
 
 
+-- select s.user_id, round(count(case when c.action='confirmed' then 1 end)/count(*),2) as confirmation_rate
+-- from signups s left join Confirmations c on s.user_id=c.user_id
+-- group by s.user_id
+
+
+
+
+
 select s.user_id, round(count(case when c.action='confirmed' then 1 end)/count(*),2) as confirmation_rate
-from signups s left join Confirmations c on s.user_id=c.user_id
+from Signups s left join Confirmations c on s.user_id=c.user_id 
 group by s.user_id
-
-
-
-
-
-
 
 
 
