bcc1855a1e904bcf25b97ad17a372a6e428317fd
Prabhat Kumar Singh <46450896+p4prabhu@users.noreply.github.com>
2025-08-02 18:05:29 -0400

    Time: 473 ms (85.1%), Space: 0B (100%) - LeetHub

--- 1161-project-employees-i/1161-project-employees-i.sql
+++ 1161-project-employees-i/1161-project-employees-i.sql
@@ -30,10 +30,26 @@
 
 
 
-select p.project_id, ifnull(round(avg(e.experience_years),2),0) average_years from Employee e join Project p
-on p.employee_id=e.employee_id
+-- select p.project_id, ifnull(round(avg(e.experience_years),2),0) average_years from Employee e join Project p
+-- on p.employee_id=e.employee_id
+-- group by p.project_id
+-- order by p.project_id
+
+
+
+
+
+
+
+
+
+
+select p.project_id, round(avg(e.experience_years),2) as average_years
+from Project p left join Employee e on p.employee_id=e.employee_id
 group by p.project_id
 order by p.project_id
+
+
 
 
 
