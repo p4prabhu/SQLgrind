67fa3051e1cb93a5a698bc6dd3031bfdf71f568b
Prabhat Kumar Singh <46450896+p4prabhu@users.noreply.github.com>
2025-08-02 20:23:25 -0400

    Time: 352 ms (62.85%), Space: 0B (100%) - LeetHub

--- 1338-queries-quality-and-percentage/1338-queries-quality-and-percentage.sql
+++ 1338-queries-quality-and-percentage/1338-queries-quality-and-percentage.sql
@@ -1,8 +1,41 @@
 # Write your MySQL query statement below
 
-select query_name,round(sum(rating/position)/count(position),2)as quality,
-round((count(case when rating<3 then 1 end)/count(position))*100,2) as poor_query_percentage
+-- select query_name,round(sum(rating/position)/count(position),2)as quality,
+-- round((count(case when rating<3 then 1 end)/count(position))*100,2) as poor_query_percentage
+-- from Queries
+-- group by query_name
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+select query_name, round(sum(rating/position)/count(position),2) as quality,
+round(count(case when rating <3 then 1 end)/count(position)*100,2) as poor_query_percentage
 from Queries
 group by query_name
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
