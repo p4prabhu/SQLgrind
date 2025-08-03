e2fde3cddb1a80a23e633ec4000f6c62db6cf298
Prabhat Kumar Singh <46450896+p4prabhu@users.noreply.github.com>
2025-08-02 22:11:02 -0400

    Time: 504 ms (92.87%), Space: 0B (100%) - LeetHub

--- 1182-game-play-analysis-iv/1182-game-play-analysis-iv.sql
+++ 1182-game-play-analysis-iv/1182-game-play-analysis-iv.sql
@@ -34,10 +34,22 @@
 -- where (player_id, date_sub(event_date, interval 1 day)) in (select player_id, min(event_date) from Activity group by player_id)
 
 
-select round(count(distinct player_id)/(select count(distinct player_id) from Activity),2) as fraction 
-from Activity
-where (player_id , date_sub(event_date, interval 1 day))  in (
-select player_id ,min(event_date) from Activity group by player_id )
+-- select round(count(distinct player_id)/(select count(distinct player_id) from Activity),2) as fraction 
+-- from Activity
+-- where (player_id , date_sub(event_date, interval 1 day))  in (
+-- select player_id ,min(event_date) from Activity group by player_id )
+
+
+
+
+
+
+
+
+select round(count(distinct player_id)/(select count(distinct player_id) from Activity),2) as fraction
+from activity 
+where (player_id, date_sub(event_date, interval 1 day)) in (select player_id, min(event_date) from activity group by player_id)
+
 
 
 
