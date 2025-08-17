7e6bbc4c50e5d42e9c43c379e4b2e93356d56992
Prabhat Kumar Singh <46450896+p4prabhu@users.noreply.github.com>
2025-08-16 23:58:59 -0400

    Time: 643 ms (96.33%), Space: 0B (100%) - LeetHub

--- 1155-product-sales-analysis-iii/1155-product-sales-analysis-iii.sql
+++ 1155-product-sales-analysis-iii/1155-product-sales-analysis-iii.sql
@@ -5,3 +5,10 @@
 select s.product_id, s.year as first_year, s.quantity, s.price  from sales s inner join (
 select product_id, min(year) first_year from Sales group by product_id) t
 on s.product_id=t.product_id and s.year=t.first_year 
+
+
+
+
+
+
+
