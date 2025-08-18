be0dd2e084b7435a98fb70447bab07d0133ed326
Prabhat Kumar Singh <46450896+p4prabhu@users.noreply.github.com>
2025-08-17 21:35:48 -0400

    Time: 699 ms (67.68%), Space: 0B (100%) - LeetHub

--- 1155-product-sales-analysis-iii/1155-product-sales-analysis-iii.sql
+++ 1155-product-sales-analysis-iii/1155-product-sales-analysis-iii.sql
@@ -2,9 +2,23 @@
 
 
 
-select s.product_id, s.year as first_year, s.quantity, s.price  from sales s inner join (
-select product_id, min(year) first_year from Sales group by product_id) t
-on s.product_id=t.product_id and s.year=t.first_year 
+-- select s.product_id, s.year as first_year, s.quantity, s.price  from sales s inner join (
+-- select product_id, min(year) first_year from Sales group by product_id) t
+-- on s.product_id=t.product_id and s.year=t.first_year 
+
+
+
+
+
+
+
+
+select product_id, year as first_year, quantity, price from sales 
+where (product_id, year) in (select product_id, min(year) from sales group by product_id)
+
+
+
+
 
 
 
